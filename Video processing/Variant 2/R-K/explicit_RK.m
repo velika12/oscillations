c0c43b982915923152478bc2a703cd6bdbf0b455
velika12 <dariatsa@gmail.com>
2016-03-06 13:01:34 +0300

    Error in x_e fixed

--- Video processing/Variant 2/R-K/explicit_RK.m
+++ Video processing/Variant 2/R-K/explicit_RK.m
@@ -7,22 +7,22 @@ g = 9.815;
 w = sqrt(c_osc/m); % 1/s
 
 tau = 0.001; % step
-tspan = [0; 160]; %s
+tspan = [0; 80]; %s
 
 % initial values
 % for rubber delta
 ksi0 = 2.51457398273736;
-x0 = ksi0 - x_e(0, tspan(1), false); % time is 0
+x0 = ksi0 - x_e(0.1, tspan(1), false); % time is 0
 u0 = [x0; -1.37497924957696];
 
-ampl = 0; % initial amplitude of the cycle (guess), m
+ampl = -6; % initial amplitude of the cycle (guess), m
 max = false;
 eps = 0.001; % difference for ampl
 
 % Matrix to save solution
 sol = zeros(3,1);
 sol(:,1) = [ tspan(1); u0 ];
-shift(1) = x_e(0, tspan(1), false);
+shift(1) = x_e(0.1, tspan(1), false);
 
 u = u0;
 t = tspan(1);
@@ -87,7 +87,7 @@ while t < tspan(2)
     t = t + tau;
 
     sol(:,i) = [ t; u ];
-    shift(i) = x_e(ampl, t, max);
+    %shift(i) = x_e(ampl, t, max);
     %fi_t = fi(c_osc, ampl, max);
     
     if sol(3,i-1)*sol(3,i) < 0
@@ -98,13 +98,21 @@ while t < tspan(2)
     end
 end
 
+i = 1;
+for t=tspan(1):tau:tspan(2)
+    shift(i) = x_e(ampl, t, max);
+    i = i+1;
+end
+    
+
 %z = (sol(1,:) - 38.66)/22.33;
 
 fin = sol(2,:) + shift;    %0.001126*z.^6 + 0.002285*z.^5 + 0.0004883*z.^4 + 0.0007962*z.^3 - 0.009244*z.^2 + 0.01409*z + 1.481;
 
 figure(5)
 %plot(sol(1,:), sol(2,:), 'b', time, deltaLenghtRubber, 'g', sol(1,:), fin, 'r')
-plot(time, deltaLenghtRubber, 'g', sol(1,:), shift, 'r', sol(1,:), fin)
+%plot(time, deltaLenghtRubber, 'g', sol(1,:), sol(2,:))
+plot(sol(1,:), shift, 'b', time, x1, 'g')
 %figure(2)
 %plot(sol(1,:), fi_t)
 grid on
--- Video processing/Variant 2/R-K/x_e.m
+++ Video processing/Variant 2/R-K/x_e.m
@@ -6,7 +6,6 @@ c_osc = 3.0603;
 g = 9.815;
 w = sqrt(c_osc/m); % 1/s
 
-x_e = m*g/c - 0.128 * exp( -c*c_osc*0.5*fi( c_osc, ampl, max)*t / ( pi*w ) );
-
+x_e = m*g/c - 0.128 * exp( -c*2*pi*w*t/( c_osc*fi( c_osc, ampl, max) ) );
 end
 
